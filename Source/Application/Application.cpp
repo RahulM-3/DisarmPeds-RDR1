e9a83f6914ae1a5f391a78804e39704f4b5505d3
RahulM-3 <rahulcode97@gmail.com>
2024-11-13 23:54:15 +0530

    worldgetallpeds function init

--- Source/Application/Application.cpp
+++ Source/Application/Application.cpp
@@ -1,8 +1,6 @@
 #include <pch.h>
 #include "debug.h"
-
-// global variable
-bool display = true;
+#include "utils.h"
 
 void Application::Initialize(HMODULE _Module)
 {
@@ -10,12 +8,32 @@ void Application::Initialize(HMODULE _Module)
 
 	ScriptRegister(_Module, []
 	{
-		std::ofstream logfile("DisarmPeds.log");
-		while (true)
-		{
-			MonitorResource();
-			WAIT(0);
-		}
+			while (true)
+			{
+				MonitorResource();
+
+				Actor player = GET_PLAYER_ACTOR(-1);
+				Vector3 playerpos = GET_OBJECT_POSITION(player);
+
+				if (Input::IsKeyJustPressed(KEY_0))
+				{
+					Actor peds[1203];
+					int pedcount = worldGetAllPeds(1203, peds);
+					logfile << "Total NPC: " << pedcount << "\n";
+					Vector3 pos;
+					for(int i=0; i<pedcount; i++)
+					{
+						pos = GET_OBJECT_POSITION(peds[i]);
+						logfile << GET_ACTOR_ENUM(peds[i]) << "\n";
+						logfile << "X:" << pos.x << " Y:" << pos.y << " Z:" << pos.z << "\n";
+						logfile << "Distance: " << getDistance(playerpos, pos) << "\n\n";
+					}	
+					logfile << "\n\n\n\n\n";
+				}
+
+				logfile.flush();
+				WAIT(0);
+			}
 	});
 }
 
